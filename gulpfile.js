533704d95d46de33821b7bebfa0192cea40a7608
ognjenvucko <ognjenvucko@gmail.com>
2017-03-08 00:52:18 +0100

    Improved gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
 const gulp = require('gulp');
-require babel = require('gulp-babel');
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
+const runSequence = require('run-sequence');
+
+gulp.task('clean-dist', () => {
+  return gulp.src('dist/*', {
+    read: false
+  }).pipe(clean({
+    force: true
+  }));
+});
 
 gulp.task('copy-libs', () => {
   return gulp.src('src/lib/*.js').pipe(gulp.dest('dist/lib'))
+});
+
+gulp.task('es6', () => {
+  return gulp.src('src/**/*.js').pipe(babel({
+    "presets": ['es2015'],
+    "ignore": [
+      "src/lib/*.js"
+    ]
+  })).pipe(gulp.dest('dist'));
+})
+
+
+gulp.task('default', (callback) => {
+  runSequence('clean-dist', ['copy-libs', 'es6'], callback);
 });
