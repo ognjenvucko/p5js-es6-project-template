73f0571cdcf99b7c8c55db101589868590af41e6
ognjenvucko <ognjenvucko@gmail.com>
2017-03-09 22:33:44 +0100

    Added demo p5js code

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,15 @@
 // Entry point of your application
 
-console.log("p5js-es6-project-template running...");
+define(['p5'], (p5) => {
+  new p5((p) => {
+    p.setup = () => {
+      p.createCanvas(400, 300);
+    }
+    p.draw = () => {
+      p.background(200);
+      p.fill(255);
+      p.textSize(22);
+      p.text("Make something great with p5js!", p.width / 2 - 160, p.height / 2);
+    }
+  });
+});
